2522a02522b5a5560ac0d8a218a301e71ea3aedd
Farhan Rabbaniansyah <m.farhan.rabbaniansyah@gmail.com>
2024-06-04 21:28:22 +0700

    commit

--- models/Log.js
+++ models/Log.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const logSchema = new mongoose.Schema({
   date: String,
+  time: String,
   tx1: { type: String, enum: ['ON', 'ON LOAD', 'ON ANTENNA', 'OFF'] },
   tx2: { type: String, enum: ['ON', 'ON LOAD', 'ON ANTENNA', 'OFF'] },
   battery1: String,
