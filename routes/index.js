70bcb6121434df15f326301d2f5ceb432fe06e32
Farhan Rabbaniansyah <m.farhan.rabbaniansyah@gmail.com>
2024-06-05 22:42:31 +0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,119 @@
-const express = require('express');
-const router = express.Router();
-const Log = require('../models/Log');
+var express = require('express');
+var router = express.Router();
+var Log = require('../models/log');
 
-const handleLogSubmission = async (req, res, logType) => {
-  const { date, tx1, tx2, battery1, battery2, temp, technician, note } = req.body;
-  const log = new Log({
-    logType,
-    date,
-    tx1,
-    tx2,
-    battery1: `${battery1} V`,
-    battery2: `${battery2} V`,
-    temp: `${temp} C`,
-    technician,
-    note
+router.post('/submit/localizer', function(req, res, next) {
+  var logData = {
+    date: req.body.date,
+    tx1: req.body.tx1,
+    tx2: req.body.tx2,
+    battery1: req.body.battery1,
+    battery2: req.body.battery2,
+    temp: req.body.temp,
+    technician: req.body.technician,
+    note: req.body.note,
+    logType: 'Localizer'
+  };
+  var log = new Log(logData);
+  log.save(function(err) {
+    if (err) return next(err);
+    res.redirect('/');
   });
-  await log.save();
-  res.redirect('/');
-};
-
-router.get('/', async (req, res) => {
-  const logs = await Log.find();
-  res.render('index', { logs });
 });
 
-router.post('/submit/localizer', async (req, res) => handleLogSubmission(req, res, 'localizer'));
-router.post('/submit/glidepath', async (req, res) => handleLogSubmission(req, res, 'glidepath'));
-router.post('/submit/middlemarker', async (req, res) => handleLogSubmission(req, res, 'middlemarker'));
-router.post('/submit/selexlocalizer', async (req, res) => handleLogSubmission(req, res, 'selexlocalizer'));
-router.post('/submit/selexglidepath', async (req, res) => handleLogSubmission(req, res, 'selexglidepath'));
-router.post('/submit/selexmiddlemarker', async (req, res) => handleLogSubmission(req, res, 'selexmiddlemarker'));
+router.post('/submit/glidepath', function(req, res, next) {
+  var logData = {
+    date: req.body.date,
+    tx1: req.body.tx1,
+    tx2: req.body.tx2,
+    battery1: req.body.battery1,
+    battery2: req.body.battery2,
+    temp: req.body.temp,
+    technician: req.body.technician,
+    note: req.body.note,
+    logType: 'Glide Path'
+  };
+  var log = new Log(logData);
+  log.save(function(err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+router.post('/submit/middlemarker', function(req, res, next) {
+  var logData = {
+    date: req.body.date,
+    tx1: req.body.tx1,
+    tx2: req.body.tx2,
+    battery1: req.body.battery1,
+    battery2: req.body.battery2,
+    temp: req.body.temp,
+    technician: req.body.technician,
+    note: req.body.note,
+    logType: 'Middle Marker'
+  };
+  var log = new Log(logData);
+  log.save(function(err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+router.post('/submit/selexlocalizer', function(req, res, next) {
+  var logData = {
+    date: req.body.date,
+    tx1: req.body.tx1,
+    tx2: req.body.tx2,
+    battery1: req.body.battery1,
+    battery2: req.body.battery2,
+    temp: req.body.temp,
+    technician: req.body.technician,
+    note: req.body.note,
+    logType: 'Selex Localizer'
+  };
+  var log = new Log(logData);
+  log.save(function(err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+router.post('/submit/selexglidepath', function(req, res, next) {
+  var logData = {
+    date: req.body.date,
+    tx1: req.body.tx1,
+    tx2: req.body.tx2,
+    battery1: req.body.battery1,
+    battery2: req.body.battery2,
+    temp: req.body.temp,
+    technician: req.body.technician,
+    note: req.body.note,
+    logType: 'Selex Glide Path'
+  };
+  var log = new Log(logData);
+  log.save(function(err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+router.post('/submit/selexmiddlemarker', function(req, res, next) {
+  var logData = {
+    date: req.body.date,
+    tx1: req.body.tx1,
+    tx2: req.body.tx2,
+    battery1: req.body.battery1,
+    battery2: req.body.battery2,
+    temp: req.body.temp,
+    technician: req.body.technician,
+    note: req.body.note,
+    logType: 'Selex Middle Marker'
+  };
+  var log = new Log(logData);
+  log.save(function(err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
