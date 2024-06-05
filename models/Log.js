2bbe4d36208f0d0e75da2560d8361c7ed5254b44
Farhan Rabbaniansyah <m.farhan.rabbaniansyah@gmail.com>
2024-06-05 22:34:34 +0700

    commit

--- models/Log.js
+++ models/Log.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const logSchema = new mongoose.Schema({
+  logType: String,
   date: String,
   tx1: String,
   tx2: String,
-  battery1: String, // Changed to String
-  battery2: String, // Changed to String
-  temp: String,     // Changed to String
+  battery1: String,
+  battery2: String,
+  temp: String,
   technician: String,
   note: String
 });
--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,33 @@ const express = require('express');
 const router = express.Router();
 const Log = require('../models/Log');
 
+const handleLogSubmission = async (req, res, logType) => {
+  const { date, tx1, tx2, battery1, battery2, temp, technician, note } = req.body;
+  const log = new Log({
+    logType,
+    date,
+    tx1,
+    tx2,
+    battery1: `${battery1} V`,
+    battery2: `${battery2} V`,
+    temp: `${temp} C`,
+    technician,
+    note
+  });
+  await log.save();
+  res.redirect('/');
+};
+
 router.get('/', async (req, res) => {
   const logs = await Log.find();
   res.render('index', { logs });
 });
 
-router.post('/submit', async (req, res) => {
-  const { date, tx1, tx2, battery1, battery2, temp, technician, note } = req.body;
-  const log = new Log({ 
-    date, 
-    tx1, 
-    tx2, 
-    battery1: `${parseFloat(battery1).toFixed(2)} V`, 
-    battery2: `${parseFloat(battery2).toFixed(2)} V`, 
-    temp: `${parseFloat(temp).toFixed(2)} C`, 
-    technician, 
-    note 
-  });
-  await log.save();
-  res.redirect('/');
-});
+router.post('/submit/localizer', async (req, res) => handleLogSubmission(req, res, 'localizer'));
+router.post('/submit/glidepath', async (req, res) => handleLogSubmission(req, res, 'glidepath'));
+router.post('/submit/middlemarker', async (req, res) => handleLogSubmission(req, res, 'middlemarker'));
+router.post('/submit/selexlocalizer', async (req, res) => handleLogSubmission(req, res, 'selexlocalizer'));
+router.post('/submit/selexglidepath', async (req, res) => handleLogSubmission(req, res, 'selexglidepath'));
+router.post('/submit/selexmiddlemarker', async (req, res) => handleLogSubmission(req, res, 'selexmiddlemarker'));
 
 module.exports = router;
