c658c25932b9af6c93cc3e1e701c28202fccd0a3
Farhan Rabbaniansyah <m.farhan.rabbaniansyah@gmail.com>
2024-06-05 15:57:02 +0700

    commit

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/index'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 80;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
