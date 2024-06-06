001a2943502ece386b4db3522a5fac5879cfb9aa
Farhan Rabbaniansyah <m.farhan.rabbaniansyah@gmail.com>
2024-06-06 23:03:59 +0700

    commit

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cookieParser());
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the public directory
 app.use(express.static('public'));
