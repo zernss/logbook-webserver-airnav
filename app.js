a8e03862ebd1b7cdfe7b96d53c3e600cf18c3f12
Farhan Rabbaniansyah <m.farhan.rabbaniansyah@gmail.com>
2024-06-06 23:05:49 +0700

    C

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-require('dotenv').config();
+
 const app = express();
 const PORT = 1025;
 
@@ -17,10 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the public directory
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/', require('./routes/index'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB Connection
 const mongoURI = 'mongodb://localhost:27017/localizer';
